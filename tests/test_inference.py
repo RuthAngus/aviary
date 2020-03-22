a0060f752294bc299805a92607789523d87132ff
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-03-22 14:18:53 -0400

    adding test that was breaking in notebook

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,10 +1,11 @@
 import numpy as np
-import aviary as av
+import pandas as pd
 import astropy.coordinates as coord
 from astropy.coordinates.builtin_frames.galactocentric \
     import get_matrix_vectors
 import astropy.units as u
-import time
+
+import aviary as av
 
 sun_xyz = [-8.122, 0, 0] * u.kpc
 sun_vxyz = [12.9, 245.6, 7.78] * u.km/u.s
@@ -92,6 +93,56 @@ def test_model():
     assert np.isclose(-7.2, pm_from_v[1].value, atol=1e-4)
     assert np.isclose(17, test_rv.value, atol=1e-4)
     assert np.isclose(17, rv_from_v.value, atol=1e-4)
+
+    df0 = pd.read_csv("../data/gaia_mc5_velocities.csv")
+    m = np.isfinite(df0.vz.values)
+    df = df0.iloc[m]
+
+    i = 0
+    df1 = df.iloc[i]
+
+    ra, dec, plx = df1.ra, df1.dec, df1.parallax
+    ra_err, dec_err, plx_err = df1.ra_error, df1.dec_error, df1.parallax_error
+    pmra, pmdec, rv = df1.pmra, df1.pmdec, df1.radial_velocity
+    pmra_err, pmdec_err, rv_err = df1.pmra_error, df1.pmdec_error, \
+            df1.radial_velocity_error
+
+    #Test the function by transforming a coordinate to galactocentric and back:
+    c = coord.SkyCoord(ra=ra*u.deg,
+                    dec=dec*u.deg,
+                    distance=(1./plx)*u.kpc,
+                    pm_ra_cosdec=pmra*u.mas/u.yr,
+                    pm_dec=pmdec*u.mas/u.yr,
+                    radial_velocity=rv*u.km/u.s)
+
+    test_galcen = c.transform_to(galcen_frame)
+    test_pm, test_rv = av.get_icrs_from_galactocentric(
+        test_galcen.data.xyz, test_galcen.velocity.d_xyz, R_gal, sun_xyz,
+        sun_vxyz)
+
+    vx = test_galcen.velocity.d_xyz[0].value
+    vy = test_galcen.velocity.d_xyz[1].value
+    vz = test_galcen.velocity.d_xyz[2].value
+
+    xyz, vxyz = av.simple_calc_vxyz(ra, dec, 1./plx, pmra, pmdec, rv)
+    _vx, _vy, _vz = vxyz
+
+    params = [vx, vy, vz, np.log(1./plx)]
+    pos = [ra, dec, 1./plx]
+    pm_from_v, rv_from_v = av.proper_motion_model(params, pos)
+    vpars = [df1.basic_vx, df1.basic_vy, df1.basic_vz,
+             np.log(1./df1.parallax)]
+    pm2, rv2 = av.proper_motion_model(vpars, pos)
+
+    assert np.isclose(pmra, test_pm[0].value, atol=1e-4)
+    assert np.isclose(pmra, pm_from_v[0].value, atol=1e-4)
+    assert np.isclose(pmra, pm2[0].value, atol=1e-4)
+    assert np.isclose(pmdec, test_pm[1].value, atol=1e-4)
+    assert np.isclose(pmdec, pm_from_v[1].value, atol=1e-4)
+    assert np.isclose(pmdec, pm2[1].value, atol=1e-4)
+    assert np.isclose(rv, test_rv.value, atol=1e-4)
+    assert np.isclose(rv, rv_from_v.value, atol=1e-4)
+    assert np.isclose(rv, rv2.value, atol=1e-4)
 
 
 test_lnlike()
