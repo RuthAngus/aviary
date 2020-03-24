bc4fd85e163de1587bb1bd8504101bc3e226eb05
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-03-24 09:46:47 -0400

    small changes to calc_velocities_in_parallel.py. now working on laptop

--- code/calc_velocities_in_parallel.py
+++ code/calc_velocities_in_parallel.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-
 import numpy as np
 import pandas as pd
 
 import matplotlib as mpl
 import emcee
+import corner
 
 from multiprocessing import Pool
 
@@ -29,14 +29,22 @@ def infer_velocity(df):
     ndim, nwalkers = 4, 16
     inits = [df["basic_vx"], df["basic_vy"], df["basic_vz"],
              np.log(1./df["parallax"])]
-    p0 = np.random.randn(nwalkers, ndim) + inits
+    p0 = np.random.randn(nwalkers, ndim)*1e-2 + inits
     sampler = emcee.EnsembleSampler(nwalkers, ndim, av.lnprob,
                                     args=(pm, pm_err, pos, pos_err))
-    sampler.run_mcmc(p0, 10000, progress=True);
+
+    # sampler.run_mcmc(p0, 10000, progress=True);
+    sampler.run_mcmc(p0, 1000, progress=True);
 
     # Extract inferred parameters and uncertainties.
-    flat_samples = sampler.get_chain(discard=5000, thin=5, flat=True)
+    # flat_samples = sampler.get_chain(discard=5000, flat=True)
+    flat_samples = sampler.get_chain(discard=500, flat=True)
     params_inferred = np.median(flat_samples, axis=0)
+
+    # labels = ["Vx", "Vy", "Vz", "D"]
+    # fig = corner.corner(flat_samples, truths=params_inferred, labels=labels);
+    # fig.savefig("corner")
+
     upper = np.percentile(flat_samples, 84, axis=0)
     lower = np.percentile(flat_samples, 16, axis=0)
     errp = upper - params_inferred
@@ -57,10 +65,10 @@ def infer_velocity(df):
         "vz_inferred_errp": errp[2],
         "vz_inferred_errm": errm[2],
         "vz_inferred_err": std[2],
-        "distance_inferred": params_inferred[3],
-        "distance_inferred_errp": errp[3],
-        "distance_inferred_errm": errm[3],
-        "distance_inferred_err": std[3]
+        "lndistance_inferred": params_inferred[3],
+        "lndistance_inferred_errp": errp[3],
+        "lndistance_inferred_errm": errm[3],
+        "lndistance_inferred_err": std[3]
         }), index=[0])
 
     df1.to_csv("velocities/{}.csv".format(df["kepid"]))
@@ -70,7 +78,7 @@ def infer_velocity(df):
 df0 = pd.read_csv("../data/gaia_mc5_velocities.csv")
 
 # For now, just run on stars with RV measurements.
-m = np.isfinite(df0["vz"].values)
+m = df0.radial_velocity.values != 0
 
 # Just the first 10 stars for now.
 df = df0.iloc[m][:10]
@@ -79,8 +87,8 @@ list_of_dicts = []
 for i in range(len(df)):
     list_of_dicts.append(df.iloc[i].to_dict())
 
-print(list_of_dicts[0])
-print(len(list_of_dicts))
+print("Running on ", len(list_of_dicts), "stars")
 
 p = Pool(24)
 list(p.map(infer_velocity, list_of_dicts))
+# infer_velocity(df.iloc[0])
