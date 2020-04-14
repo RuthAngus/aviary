7095cf18f32ea4385a34c1416cabc8a10a7f1ec7
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-04-14 13:07:18 -0400

    adding some things to sh file

--- code/run_kepids.sh
+++ code/run_kepids.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# Request three nodes that will not be allocated to others for the duration of the run.
+#SBATCH -N1 --exclusive
+
+# Find out what nodes we were assigned.
+srun hostname
 
 export PATH="$HOME/miniconda/bin:$PATH"
 python run_kepids.py
