8ebf2bb28730b93c8e1e51015fd3fc64de3f276f
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2020-04-16 08:12:56 -0400

    typo in units

--- aviary/pymc3_functions_one_star.py
+++ aviary/pymc3_functions_one_star.py
@@ -30,7 +30,7 @@ def deg_to_rad(deg):
 # Solar coords
 sun_xyz = [-8.122, 0, 0] * u.kpc
 sun_vxyz = [12.9, 245.6, 7.78] * u.km/u.s
-sun_vxyzCD = coord.CartesianDifferential(12.9, 245.6, 7.78, u.km/s)
+sun_vxyzCD = coord.CartesianDifferential(12.9, 245.6, 7.78, u.km/u.s)
 
 galcen_frame = coord.Galactocentric(galcen_distance=np.abs(sun_xyz[0]),
                                     galcen_v_sun=sun_vxyzCD,
